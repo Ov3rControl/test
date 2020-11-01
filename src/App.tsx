b7fc5540063e8d30fb320dd1814780956ccfc87c
Mohamed Adel <mohamedibrahimrc@gmail.com>
2020-11-01 19:47:06 +0200

    :wrench: app main configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from "react";
 import "./App.css";
+import { routes } from "./router/config";
+import Router from "./router/Router";
 
 const App: FunctionComponent = (): JSX.Element => {
-  return <div className="App"></div>;
+  return <Router routes={routes} />;
 };
 
 export default App;
